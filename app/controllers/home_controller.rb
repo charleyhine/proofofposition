51dd30a8bd390b6891832aa15c8b4470b417a56c
Charley Hine <charles.hine@gmail.com>
2014-07-03 08:39:46 -0700

    Refactored transaction send

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,27 +6,17 @@ class HomeController < ApplicationController
   end
   
   def inside
-    if current_user.stampable?
-      @stampable = true
-    else
-      @last_tx = current_user.transactions.last
-    end
+      @stampable = current_user.stampable?
+      @last_tx ||= current_user.transactions.last
   end
   
   def stamp
     if current_user.stampable?
-      from_address = '1Bj5UVzWQ84iBCUiy5eQ1NEfWfJ4a3yKG1'
-      private_key = ENV['PRIVATE_KEY']
-      to_address = '1JJynffTaq3bWcWTXnC4P68VMeNNHdVmMy'
       metadata = '@' + current_user.username + ',' + (sprintf "%.6f", @lat_lng[0]) + ',' + (sprintf "%.6f", @lat_lng[1])
-    
-      hex = Chain.build_metadata_transaction(from_address, private_key, to_address, metadata)
-    
+      
       tx = Transaction.new
-      tx.user = current_user
+      tx.user = user
       tx.metadata = metadata
-      tx.response = Chain.send_transaction(hex) if Rails.env.production?
-      tx.response = "Test" if Rails.env.development?
       tx.save
     end
     
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,18 @@ class Transaction < ActiveRecord::Base
   belongs_to :user
   
   validates :user, presence: true
+  validates :metadata, presence: true
+  
+  before_create :bitcoin_transaction
+  
+  def bitcoin_transaction
+    from_address = '1Bj5UVzWQ84iBCUiy5eQ1NEfWfJ4a3yKG1'
+    private_key = ENV['PRIVATE_KEY']
+    to_address = '1JJynffTaq3bWcWTXnC4P68VMeNNHdVmMy'
+
+    hex = Chain.build_metadata_transaction(from_address, private_key, to_address, self.metadata)
+    
+    self.response = Chain.send_transaction(hex) if Rails.env.production?
+    self.response = "Testing" if Rails.env.development?  
+  end
 end
