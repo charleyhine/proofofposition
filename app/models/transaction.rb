dff446e5279f2498c74b6b061dc1d5b0ffbe5b1c
Charley Hine <charles.hine@gmail.com>
2014-07-31 10:55:35 -0700

    From address change

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -7,13 +7,18 @@ class Transaction < ActiveRecord::Base
   before_create :bitcoin_transaction
   
   def bitcoin_transaction
-    from_address = '1Bj5UVzWQ84iBCUiy5eQ1NEfWfJ4a3yKG1'
+    from_address = '18Qi4yEQtVvSzDrapG6EWPFSumwdLWvZbJ'
     private_key = ENV['PRIVATE_KEY'] # Wallet import format (starts with a 5)
     to_address = '1JJynffTaq3bWcWTXnC4P68VMeNNHdVmMy'
+    
+    p from_address
+    p private_key
+    p to_address
+    p self.metadata
 
     hex = Chain.build_metadata_transaction(from_address, private_key, to_address, self.metadata)
     
-    self.response = Chain.send_transaction(hex) if Rails.env.production?
-    self.response = "Testing" if Rails.env.development?  
+    #self.response = Chain.send_transaction(hex) if Rails.env.production?
+    #self.response = "Testing" if Rails.env.development?  
   end
 end
